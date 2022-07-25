66bbe8ec5c7b29a5277438ccde2ab36f9280ce64
Mahmood Choopani <choopani.m81@gmail.com>
2022-07-25 15:05:26 +0430

    make server multi threaded and test it with run with 10 parallel ran client

--- client.py
+++ client.py
@@ -1,28 +1,43 @@
+import random
 import socket
+import threading
+from datetime import datetime
+
 from helpers import get_message, send_message
 
 
-def start_messaging(HOST, PORT):
+def start_messaging(HOST, PORT, thread_id):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        messaging(s)
+        messaging(s, thread_id)
 
 
-def messaging(conn):
+def messaging(conn, current_thread_id):
     with conn:
         # welcome_message = get_message(conn)
         # print(welcome_message)
-        while True:
+        for _ in range(2):
             try:
-                message = input('enter a message to echo: ')
+                # message = input('enter a message to echo: ')
+                message = str(random.Random().randint(a=1, b=50))
                 send_message(conn, message)
-                print(get_message(conn))
-                if message == 'exit':
-                    break
+                print(f'client {current_thread_id} logs: {get_message(conn)}')
             except Exception as e:
                 print(f'Exception: {str(e)}')
                 break
+        send_message(conn, 'exit')
+        get_message(conn)
 
 
 if __name__ == '__main__':
-    start_messaging('127.0.0.1', 12345)
+    start = datetime.now().timestamp()
+    thrds = []
+    for i in range(10):
+        thr = threading.Thread(target=start_messaging, args=('127.0.0.1', 12345, i))
+        thr.start()
+        thrds.append(thr)
+
+    for thr in thrds:
+        thr.join()
+    end = datetime.now().timestamp()
+    print(f'Process time is : {end - start}')
--- server.py
+++ server.py
@@ -1,25 +1,32 @@
 import socket
-
+import threading
 from helpers import send_message, get_message
-
+import time
 
 def start_listening(HOST, PORT):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        conn, addr = s.accept()
-        messaging(conn, addr)
+        while True:
+            conn, addr = s.accept()
+            this_thread = threading.Thread(target=messaging, args=(conn, addr))
+            this_thread.start()
 
 
 def messaging(conn, addr):
     with conn:
         # send_message(conn, f'hi {addr}')
         while True:
-            print('waiting for client message...')
-            message_from_client = get_message(conn)
-            print(message_from_client)
-            result = send_message(conn, message_from_client)
-            if message_from_client == 'exit' or not result:
+            try:
+                print('waiting for client message...')
+                message_from_client = get_message(conn)
+                time.sleep(2)
+                print(message_from_client)
+                result = send_message(conn, message_from_client)
+                if message_from_client == 'exit' or not result:
+                    break
+            except Exception as e:
+                print(f'{addr} closed because {str(e)}')
                 break
 
 
