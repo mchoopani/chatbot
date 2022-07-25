e108039697f6be3e05b67d981016cd5a0be5cde5
Mahmood Choopani <choopani.m81@gmail.com>
2022-07-25 17:43:14 +0430

    refactor server codes

--- server.py
+++ server.py
@@ -4,31 +4,44 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 
-def start_listening(HOST, PORT):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        with ThreadPoolExecutor(max_workers=5) as executor:
+class Server:
+
+    instance = None
+
+    @classmethod
+    def get_instance(cls, HOST, PORT, multi_client_count):
+        if cls.instance is None:
+            cls.instance = Server(HOST, PORT, multi_client_count)
+        return cls.instance
+
+    def __init__(self, HOST, PORT, multi_client_count):
+        self.HOST = HOST
+        self.PORT = PORT
+        self.multi_client_count = multi_client_count
+
+    def start_listening(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.HOST, self.PORT))
+            s.listen()
+            with ThreadPoolExecutor(max_workers=self.multi_client_count) as executor:
+                while True:
+                    conn, addr = s.accept()
+                    executor.submit(self.messaging, conn, addr)
+
+    def messaging(self, conn, addr):
+        with conn:
             while True:
-                conn, addr = s.accept()
-                executor.submit(messaging, conn, addr)
-
-
-def messaging(conn, addr):
-    with conn:
-        while True:
-            try:
-                print('waiting for client message...')
-                message_from_client = get_message(conn)
-                time.sleep(1)
-                print(message_from_client)
-                result = send_message(conn, message_from_client)
-                if message_from_client == 'exit' or not result:
+                try:
+                    message_from_client = get_message(conn)
+                    time.sleep(1)
+                    result = send_message(conn, message_from_client)
+                    if message_from_client == 'exit' or not result:
+                        break
+                except Exception as e:
+                    print(f'{addr} closed because {str(e)}')
                     break
-            except Exception as e:
-                print(f'{addr} closed because {str(e)}')
-                break
 
 
 if __name__ == '__main__':
-    start_listening('127.0.0.1', 12346)
+    server = Server.get_instance('127.0.0.1', 12346,5)
+    server.start_listening()
