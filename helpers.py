e723bf30ccb153d3ee9103e1d3b44841027bedf9
Mahmood Choopani <choopani.m81@gmail.com>
2022-07-25 17:58:04 +0430

    add decorator for function run time calculating

--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def get_message(conn):
     return conn.recv(1024).decode('utf-8')
 
@@ -9,3 +12,13 @@ def send_message(conn, message):
     except Exception as e:
         print(str(e))
         return False
+
+
+def time_calculator(func):
+    def wrap(*args, **kwargs):
+        start = datetime.now().timestamp()
+        return_value = func(*args, **kwargs)
+        end = datetime.now().timestamp()
+        print(f'This function run time is : {end-start}')
+        return return_value
+    return wrap
